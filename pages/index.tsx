5969543b5301c970e4372e54ab5b9c757b49bf06
David Bryant <nedink@gmail.com>
2022-08-26 23:15:42 -0400

    add default styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-// import '' '../''.module.css'
 
 export default function Home() {
   return (
-    <div className='text-2xl text-red-100'>
+    <div className=''>
 Hello World
     </div>
   )
