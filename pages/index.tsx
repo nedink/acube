f83013220bd705753163c3b7aedf0b7a13b978ef
David Bryant <nedink@gmail.com>
2022-08-26 22:57:12 -0400

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+// import '' '../''.module.css'
+
+export default function Home() {
+  return (
+    <div className='text-2xl text-red-100'>
+Hello World
+    </div>
+  )
+}
