87ab7ce2188884af108573ea4d9abfccb885e2a1
David Bryant <nedink@gmail.com>
2022-09-21 16:30:38 -0400

    use Theo's CORS fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,20 @@ function MyApp({ Component, pageProps }: AppProps) {
 import { withTRPC } from '@trpc/next'
 import type { AppRouter } from '@/backend/rounter'
 
+function getBaseUrl() {
+  if (typeof window === 'undefined') return "" // browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}/api/trpc` // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+}
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = `${getBaseUrl()}/api/trpc`;
     return {
       url,
       /**
